2a0281543e8d163c15fc1106019f9bddfeb87103
dhakshaini <52372795+dhakshaini@users.noreply.github.com>
2021-07-28 15:10:53 +0530

    Commit new file

---
+++ User.java
@@ -0,0 +1,12 @@
+package com.lti.demo;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!!");
+		System.out.println("Hello world!!");
+
+	}
+	
+}
